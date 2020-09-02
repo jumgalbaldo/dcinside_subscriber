d52f1ddcee57ae64f36397366052eb4fae4fdc7a
jumgalbaldo <jumgalbaldo@gmail.com>
2020-09-02 10:54:01 +0900

    fix bug

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const { MongoClient } = require('mongodb')
 
    async function work() {
       try {
-         const savedPosts = await postsCollection.find({}, { limit: 10, sort: { $natural: -1 } }).toArray()
+         const savedPosts = await postsCollection.find({}, { limit: 10, sort: { no: -1 } }).toArray()
          if (savedPosts.length === 0) {
             const posts = await site.getPosts({ limit: 1 })
             if (posts.length > 0) {
