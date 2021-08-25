3d9b65fafc37182a1bbfd19ca8dd18d9af621094
jumgalbaldo <jumgalbaldo@gmail.com>
2021-08-25 21:27:37 +0900

    recommendRatio / unallowedRegex

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ const { MongoClient } = require('mongodb')
          db = client.db('dcinside_subscriber'),
          envCollection = db.collection('env'),
          postsCollection = db.collection('posts'),
-         env = await envCollection.findOne({}, { sort: { $natural: -1 } })
+         env = {
+            recentPostsLimit: 30,
+            savedPostsLimit: 30,
+            unallowedRegex: '백업금지',
+            minRecommendRatio: 0.2,
+            ...await envCollection.findOne({}, { sort: { $natural: -1 } })
+         }
 
-   if (!env) {
+   if (!env.board) {
       throw new Error('can not find env')
    }
 
@@ -37,19 +43,27 @@ const { MongoClient } = require('mongodb')
                   !savedPosts.find(({ no: no_ }) => no === no_)
                )
          for (let post of posts.reverse()) {
+            let page
             try {
-               const page = await browser.newPage()
+               page = await browser.newPage()
                await page.setUserAgent('Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.81 Mobile Safari/537.36')
                await page.setViewport({ width: 600, height: 2048 })
                await page.goto(post.link, { waitUntil: 'networkidle2' })
                await page.evaluate(() => $('#div_adnmore_area').hide())
+
+               const [text, recommendRatio] = await page.evaluate(() => [$('.thum-txtin').html(), parseInt($('#recomm_btn_member').html()) / parseInt($('#recomm_btn').html())])
+               if (text.match(env.unallowedRegex)) {
+                  throw new Error('unallowed')
+               }
+               else if (recommendRatio <= env.minRecommendRatio) {
+                  throw new Error('low recommendRatio')
+               }
+
                await page.emulateMediaType('screen')
 
                const pdf = await page.pdf({ width: 1024, height: 2048 }),
                      image = await page.screenshot({ type: 'jpeg', clip: { x: 0, y: 180, width: 600, height: 600 } }),
                      thumbnail = await sharp(image).resize({ width: 240 }).toBuffer()
-
-               await page.close()
 
                const formData = new FormData(),
                      filename = sanitize(post.title) || '_'
@@ -63,13 +77,20 @@ const { MongoClient } = require('mongodb')
                   maxContentLength: Infinity,
                   maxBodyLength: Infinity
                })
+
+               await postsCollection.insertOne(post)
             }
             catch (err) {
                console.error(err)
+
+               await postsCollection.insertOne({
+                  ...post,
+                  err: err.toString()
+               })
             }
-
-            await postsCollection.insertOne(post)
-
+            finally {
+               await page?.close()
+            }
             console.log(post)
          }
       }
